f73b154d2d59e7b388b2b650729def2bf1b1d7f8
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-09-30 21:23:00 -0700

    pdf to png

--- converter.py
+++ converter.py
@@ -1,16 +1,20 @@
-from PIL import Image
+import fitz
 
-def convert_jpg_to_png(input_path, output_path):
-    try:
-        # Open the input JPG image
-        with Image.open(input_path) as img:
-            # Convert and save as a PNG image
-            img.save(output_path, 'PNG')
-        print(f"Conversion successful. Saved as {output_path}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def convert_pdf_to_png(pdf_path, output_png_path, dpi=300):
+    pdf_document = fitz.open(pdf_path)
+    page = pdf_document.load_page(0)  # Load the first page (index 0)
+    
+    # Adjust the DPI for higher quality (e.g., 300 DPI)
+    image = page.get_pixmap(matrix=fitz.Matrix(dpi/72, dpi/72))
 
-if __name__ == "__main__":
-    input_path = "input_example.jpg"  # Change this to the path of your JPG image
-    output_path = "output.png"  # Change this to the desired output PNG file path
-    convert_jpg_to_png(input_path, output_path)
+    # Save the pixmap as a PNG file
+    image.save(output_png_path)
+
+    # Close the PDF document
+    pdf_document.close()
+
+# Example usage
+input_pdf_path = 'input2_example.pdf'  # Path to the input PDF file
+output_png_path = 'output.png'  # Path to save the output PNG file
+
+convert_pdf_to_png(input_pdf_path, output_png_path, dpi=300)
